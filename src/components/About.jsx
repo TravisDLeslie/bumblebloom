8cf989d396223cc2b8bbf1d2f967dccb8f42e3a9
travisdleslie <hello@travisleslie.com>
2024-07-15 17:16:09 -0600

    more more

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -13,7 +13,7 @@ const About = () => {
             <img src={icon} alt="About Us" className="absolute top-[-50px] left-[0px] md:top-[-50px] md:left-[-50px] w-24 h-24 p-6 bg-bumbleYellow border rounded-2xl" />
             <img src={aboutImage} alt="About Us" className="w-[500px] h-[500px] object-cover rounded-2xl bg-gray-200" />
           </div>
-          <div className="text-white ml-8 mt-4 w-[400px] md:w-[600px]">
+          <div className="text-white ml-8 mt-4 w-full md:w-[600px]">
             <h3 className="text-2xl font-ribeye font-bold">BRYNNA LESLIE <span className="italic font-poppins text-xl font-normal">- 15+ yrs experience</span></h3>
             <p className="mt-4 font-normal text-base">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus luctus urna sed urna ultricies ac tempor dui sagittis.
